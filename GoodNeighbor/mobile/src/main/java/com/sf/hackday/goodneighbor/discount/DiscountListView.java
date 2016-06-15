b8c9f40fc05879ec362e2b5483b65c8f6c45ae66
Stephen Young <stephenyoung@Stephens-MacBook-Pro-4.local>
2016-06-15 17:54:33 -0400

    list stuff

--- GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/discount/DiscountListView.java
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/discount/DiscountListView.java
@@ -1,37 +1,45 @@
 package com.sf.hackday.goodneighbor.discount;
 
 import android.app.ListActivity;
-import
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.ArrayAdapter;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.app.Activity;
+import android.view.Menu;
+import com.sf.hackday.goodneighbor.R;
 
 /**
  * Created by stephenyoung on 6/15/16.
  */
 public class DiscountListView extends ListActivity {
-
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     ArrayList<String> listItems=new ArrayList<String>();
 
     //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
     ArrayAdapter<String> adapter;
 
-    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
-    int clickCounter=0;
-
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.main);
-        adapter=new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
-        setListAdapter(adapter);
-    }
+    public void onCreate(Bundle savedInstanceState) {
 
-    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
-    public void addItems(View v) {
-        listItems.add("Clicked : "+clickCounter++);
-        adapter.notifyDataSetChanged();
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.content_discounts);
+//        ArrayAdapter adapter2 =
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.content_discounts, R.id.labelDiscount, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+//        super.onCreate(icicle);
+//        setContentView(R.layout.content_discounts);
+//        listItems.add("Hello");
+//        listItems.add("goodbe");
+//        adapter=new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                listItems);
+//        setListAdapter(adapter);
     }
-}
 
 }
