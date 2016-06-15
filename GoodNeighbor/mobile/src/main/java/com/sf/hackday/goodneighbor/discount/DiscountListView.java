183485618c3927027420134215803ab91e632946
Stephen Young <stephenyoung@Stephens-MacBook-Pro-4.local>
2016-06-15 15:42:53 -0400

    discount list

---
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/discount/DiscountListView.java
@@ -0,0 +1,37 @@
+package com.sf.hackday.goodneighbor.discount;
+
+import android.app.ListActivity;
+import
+
+/**
+ * Created by stephenyoung on 6/15/16.
+ */
+public class DiscountListView extends ListActivity {
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter=0;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Clicked : "+clickCounter++);
+        adapter.notifyDataSetChanged();
+    }
+}
+
+}
