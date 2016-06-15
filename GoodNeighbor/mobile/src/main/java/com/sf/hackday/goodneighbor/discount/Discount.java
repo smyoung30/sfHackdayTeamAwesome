86c260f02810e3c0ae21ea1e8b1f12c28dbe3dfa
Stephen Young <stephenyoung@Stephens-MacBook-Pro-4.local>
2016-06-15 12:54:15 -0400

    Discount Object

--- GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/discount/Discount.java
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/discount/Discount.java
@@ -4,4 +4,31 @@ package com.sf.hackday.goodneighbor.discount;
  * Created by stephenyoung on 6/15/16.
  */
 public class Discount {
+    String name;
+    int id;
+    double discountAmount;
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
