2340c2ebe88309eb14741a44c67d4196aa0256d4
svermeyen1 <slayaman1>
2016-06-15 19:32:28 -0400

    Update discounts

--- GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/activities/DashboardActivity.java
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/activities/DashboardActivity.java
@@ -7,8 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
 import com.sf.hackday.goodneighbor.R;
 import com.sf.hackday.goodneighbor.presenters.GoodNeighborPresenter;
 import com.sf.hackday.goodneighbor.views.ClickListener;
@@ -62,8 +61,8 @@ public class DashboardActivity extends AppCompatActivity {
      *
      * @param v view
      */
-    public void gotoPolicyActivity(View v) {
-        Intent intent = new Intent(this, PolicyActivity.class);
+    public void gotoDiscountsActivity(View v) {
+        Intent intent = new Intent(this, DiscountsActivity.class);
         startActivity(intent);
     }
 
--- GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/activities/DiscountsActivity.java
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/activities/DiscountsActivity.java
@@ -1,20 +1,23 @@
 package com.sf.hackday.goodneighbor.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.sf.hackday.goodneighbor.R;
 
-public class PolicyActivity extends AppCompatActivity {
+public class DiscountsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_policy);
+        setContentView(R.layout.activity_discounts);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
@@ -42,6 +45,7 @@ public class PolicyActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
 
+
+    }
 }
