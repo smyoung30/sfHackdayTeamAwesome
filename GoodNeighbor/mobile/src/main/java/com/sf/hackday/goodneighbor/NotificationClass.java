3934e4fb619aa1e52aa2954390877116790ebb0a
mlazo007@fiu.edu <mlazo007@fiu.edu>
2016-06-15 21:42:27 -0400

    Notification classes and layouts

---
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/NotificationClass.java
@@ -0,0 +1,15 @@
+package com.sf.hackday.goodneighbor;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by Michael on 6/15/2016.
+ */
+public class NotificationClass extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_class_layout);
+    }
+}
