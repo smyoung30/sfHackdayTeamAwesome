a38d6ac88ca6726a36f063e39d7a8ea7f7013f3d
Stephen Young <stephenyoung@Stephens-MacBook-Pro-4.local>
2016-06-16 07:29:41 -0400

    Merge branch 'master' of https://github.com/smyoung30/sfHackdayTeamAwesome

--- GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/NotificationClass.java
+++ GoodNeighbor/mobile/src/main/java/com/sf/hackday/goodneighbor/NotificationClass.java
@@ -1,7 +1,12 @@
 package com.sf.hackday.goodneighbor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sf.hackday.goodneighbor.activities.DiscountsActivity;
+
 /**
  * Created by Michael on 6/15/2016.
  */
@@ -10,6 +15,12 @@ public class NotificationClass extends Activity {
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discounts);
+        setContentView(R.layout.notification_class_layout);
     }
+
+    public void gotoDiscountsActivity(View v) {
+        Intent intent = new Intent(this, DiscountsActivity.class);
+        startActivity(intent);
+    }
+
 }
